58d4639e521c38348889793951cc39db58f4b549
Shadi <TheSisb@users.noreply.github.com>
2016-11-07 15:15:40 -0800

    Cleanup and fixes

--- index.js
+++ index.js
@@ -6,55 +6,93 @@ import {
   View,
   Animated,
   Dimensions,
-  PanResponder,
-  TouchableWithoutFeedback
+  PanResponder
 } from 'react-native';
 
-let HEIGHT = Dimensions.get('window').height;
-var Row = React.createClass({
+const HEIGHT = Dimensions.get('window').height;
+const WRAPPER_REF = 'WRAPPER_REF';
+const LIST_REF = 'LIST_REF';
+const VIEW_REF = 'VIEW_REF';
+const NONE_INDEX = -1;
+
+const Row = React.createClass({
   _data: {},
-  shouldComponentUpdate: function(props) {
-    if (props.hovering !== this.props.hovering) return true;
-    if (props.active !== this.props.active) return true;
-    if (props.rowData.data !== this.props.rowData.data) return true;
-    if (props.rowHasChanged) return props.rowHasChanged(props.rowData.data, this._data);
+
+  shouldComponentUpdate(props) {
+    if (
+      props.hovering !== this.props.hovering ||
+      props.hoverIndex !== this.props.hoverIndex ||
+      props.active !== this.props.active ||
+      props.rowData.data !== this.props.rowData.data
+    ) {
+      return true;
+    }
+
+    if (props.rowHasChanged) {
+      return props.rowHasChanged(props.rowData.data, this._data);
+    }
     return false;
   },
-  handleLongPress: function(e) {
-    this.refs.view.measure((frameX, frameY, frameWidth, frameHeight, pageX, pageY) => {
-      let layout = {frameX, frameY, frameWidth, frameHeight, pageX, pageY};
+
+  handleLongPress(e) {
+    this.refs[VIEW_REF].measure((frameX, frameY, frameWidth, frameHeight, pageX, pageY) => {
       this.props.onRowActive({
-        layout: layout,
+        layout: {frameX, frameY, frameWidth, frameHeight, pageX, pageY},
         touch: e.nativeEvent,
         rowData: this.props.rowData
       });
     });
   },
-  componentDidUpdate: function(props) {
-    //Take a shallow copy of the active data. So we can do manual comparisons of rows if needed.
+
+  componentDidUpdate(props) {
+    // Take a shallow copy of the active data. So we can do manual comparisons of rows if needed.
     if (props.rowHasChanged) {
       this._data = Object.assign({}, props.rowData.data);
     }
   },
-  measure: function() {
-    return this.refs.view.measure.apply(this, Array.from(arguments));
+
+  measure() {
+    return this.refs[VIEW_REF].measure.apply(this, arguments);
   },
-  render: function() {
-    let layout = this.props.list.layoutMap[this.props.rowData.index];
+
+  render() {
     let activeData = this.props.list.state.active;
 
-    let activeIndex = activeData ? Number(activeData.rowData.index) : -5;
+    let activeIndex = activeData ? Number(activeData.rowData.index) : NONE_INDEX;
     let shouldDisplayHovering = activeIndex !== this.props.rowData.index;
-    let Row = React.cloneElement(this.props.renderRow(this.props.rowData.data, this.props.rowData.section, this.props.rowData.index, null, this.props.active), {sortHandlers: {onLongPress: this.handleLongPress, onPressOut: this.props.list.cancel}, onLongPress: this.handleLongPress, onPressOut: this.props.list.cancel});
-    return <View onLayout={this.props.onRowLayout} style={this.props.active && this.props.list.state.hovering ? {height: 0.01, opacity: 0} : null} ref="view">
-          {this.props.hovering && shouldDisplayHovering ? this.props.activeDivider : null}
-          {this.props.active && this.props.list.state.hovering && this.props._legacySupport ? null : Row}
-        </View>
+
+    let Row = React.cloneElement(
+      this.props.renderRow(
+        this.props.rowData.data,
+        this.props.rowData.section,
+        this.props.rowData.index,
+        null,
+        this.props.active
+      ),
+      {
+        sortHandlers: {
+          onLongPress: this.handleLongPress,
+          onPressOut: this.props.list.cancel
+        },
+        onLongPress: this.handleLongPress,
+        onPressOut: this.props.list.cancel
+      }
+    );
+
+    return (
+      // Height is 0.01 because https://github.com/facebook/react-native/issues/3397
+      <View onLayout={this.props.onRowLayout}
+            style={this.props.active && this.props.list.state.hovering ? {height: 0.01, opacity: 0} : null}
+            ref={VIEW_REF}>
+        {this.props.hovering && shouldDisplayHovering ? this.props.activeDivider : null}
+        {Row}
+      </View>
+    );
   }
 });
 
-var SortRow = React.createClass({
-  getInitialState: function() {
+const SortRow = React.createClass({
+  getInitialState() {
     let layout = this.props.list.state.active.layout;
     let wrapperLayout = this.props.list.wrapperLayout;
 
@@ -63,83 +101,112 @@ var SortRow = React.createClass({
         position: 'absolute',
         left: 0,
         right: 0,
-        opacity: .2,
-        height: layout.frameHeight,
+        opacity: .25,
         overflow: 'hidden',
         backgroundColor: 'transparent',
-        marginTop: layout.pageY - wrapperLayout.pageY //Account for top bar spacing
+
+        height: layout.frameHeight, // Row height
+        marginTop: layout.pageY - wrapperLayout.frameY - layout.frameHeight
       }
-    }
+    };
   },
-  render: function() {
-    let handlers = this.props.panResponder.panHandlers;
+
+  render() {
+    const {rowData, sortRowStyle, list} = this.props;
+
     return (
-      <Animated.View ref="view" style={[this.state.style, this.props.sortRowStyle, this.props.list.state.pan.getLayout()]}>
-        {this.props.renderRow(this.props.rowData.data, this.props.rowData.section, this.props.rowData.index, null, true)}
+      <Animated.View ref={VIEW_REF}
+                     style={[this.state.style, sortRowStyle, list.state.pan.getLayout()]}>
+        {this.props.renderRow(rowData.data, rowData.section, rowData.index, null, true)}
       </Animated.View>
     );
   }
 });
 
-var SortableListView = React.createClass({
+const SortableListView = React.createClass({
   mixins: [TimerMixin],
-  getInitialState:function() {
 
-    let currentPanValue = {x: 0, y: 0};
+  getInitialState() {
+    const initialPanValue = {x: 0, y: 0};
+    let itemHeight = 0;
 
     this.state = {
-      ds: new ListView.DataSource({rowHasChanged: (r1, r2) => {
+      dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => {
         if (this.props.rowHasChanged) return this.props.rowHasChanged(r1, r2);
         return false;
       }}),
       active: false,
       hovering: false,
-      pan: new Animated.ValueXY(currentPanValue)
+      hoverIndex: NONE_INDEX,
+      pan: new Animated.ValueXY(initialPanValue)
     };
+
     this.listener = this.state.pan.addListener(e => this.panY = e.y);
+
     let onPanResponderMoveCb = Animated.event([null, {
-           dx: this.state.pan.x, // x,y are Animated.Value
-           dy: this.state.pan.y,
-      }]);
+       dx: this.state.pan.x, // x,y are Animated.Value
+       dy: this.state.pan.y,
+    }]);
 
     this.state.panResponder = PanResponder.create({
       onStartShouldSetPanResponder: (e) => true,
       onMoveShouldSetPanResponderCapture: (e, a) => {
-        //Only capture when moving vertically, this helps for child swiper rows.
+        // Only capture when moving vertically, this helps for child swiper rows.
         let vy = Math.abs(a.vy);
         let vx = Math.abs(a.vx);
-
-        return (vy) > vx  && this.state.active;
+        return vy > vx && this.state.active;
       },
+
       onPanResponderMove: (evt, gestureState) => {
         gestureState.dx = 0;
-        this.moveY = gestureState.moveY;
+        this.moveY = gestureState.moveY + (itemHeight * .5);
         onPanResponderMoveCb(evt, gestureState);
-       },
-
-       onPanResponderGrant: (e, gestureState) => {
-          this.moved = true;
-          this.props.onMoveStart &&  this.props.onMoveStart();
-          this.state.pan.setOffset(currentPanValue);
-          this.state.pan.setValue(currentPanValue);
       },
-      onPanResponderRelease: (e) => {
 
+      onPanResponderGrant: (evt, gestureState) => {
+        const {layout} = this.state.active;
+        if (layout) {
+          itemHeight = layout.frameHeight;
+        }
+        this.moved = true;
+        this.state.pan.setOffset(initialPanValue);
+        this.state.pan.setValue(initialPanValue);
+        this.props.onMoveStart && this.props.onMoveStart();
+      },
+
+      onPanResponderRelease: (evt) => {
         this.moved = false;
         this.props.onMoveEnd && this.props.onMoveEnd();
+
         if (!this.state.active) {
-          if (this.state.hovering) this.setState({hovering: false});
+          if (this.state.hovering) {
+            this.setState({
+              hovering: false,
+              hoverIndex: NONE_INDEX
+            });
+          }
           this.moveY = null;
           return;
         }
-        let itemHeight = this.state.active.layout.frameHeight;
+
+        itemHeight = this.state.active.layout.frameHeight;
         let fromIndex = this.order.indexOf(this.state.active.rowData.index);
-        let toIndex = this.state.hovering === false ?  fromIndex : Number(this.state.hovering);
+        let toIndex = this.state.hovering === false ? fromIndex : this.state.hoverIndex;
         let up = toIndex > fromIndex;
+
         if (up) {
           toIndex--;
         }
-        if (toIndex === fromIndex) return this.setState({active: false, hovering: false});
+
+        if (toIndex === fromIndex) {
+          return this.setState({active: false, hovering: false, hoverIndex: NONE_INDEX});
+        }
+
+        // Don't animate fromIndex -> toIndex after dropping, just drop.
+        LayoutAnimation.configureNext({
+          duration: 0
+        });
+
         let args = {
           row: this.state.active.rowData,
           from: fromIndex,
@@ -147,54 +214,58 @@ var SortableListView = React.createClass({
         };
 
         this.props.onRowMoved && this.props.onRowMoved(args);
-        if (this.props._legacySupport) { //rely on parent data changes to set state changes
-          //LayoutAnimation.easeInEaseOut()
-          this.state.active = false;
-          this.state.hovering = false;
-        } else {
-          this.setState({
-            active: false,
-            hovering: false
-          });
-        }
+
+        this.setState({
+          active: false,
+          hovering: false,
+          hoverIndex: NONE_INDEX,
+          moveY: null
+        });
 
         let MAX_HEIGHT = Math.max(0, this.scrollContainerHeight - this.listLayout.height + itemHeight);
+
         if (this.scrollValue > MAX_HEIGHT) {
           this.scrollResponder.scrollTo({y: MAX_HEIGHT});
         }
-
-        this.state.active = false;
-        this.state.hovering = false;
-        this.moveY = null;
       }
-     });
+    });
 
     return this.state;
   },
-  cancel: function() {
+
+  cancel() {
     if (!this.moved) {
       this.setState({
         active: false,
-        hovering: false
+        hovering: false,
+        hoverIndex: NONE_INDEX
       });
     }
   },
-  componentDidMount: function() {
-    setTimeout(()=>{
-      this.scrollResponder = this.refs.list.getScrollResponder();
-      this.refs.wrapper.measure((frameX, frameY, frameWidth, frameHeight, pageX, pageY) => {
 
-        let layout = {frameX, frameY, frameWidth, frameHeight, pageX, pageY};
-        this.wrapperLayout = layout;
+  scrollTo(...args) {
+    this.scrollResponder.scrollTo.apply(this.scrollResponder, args);
+  },
+
+  componentDidMount() {
+    this.setTimeout(() => {
+      this.scrollResponder = this.refs[LIST_REF].getScrollResponder();
+      this.refs[WRAPPER_REF].measure((frameX, frameY, frameWidth, frameHeight, pageX, pageY) => {
+        this.wrapperLayout = {frameX, frameY, frameWidth, frameHeight, pageX, pageY};
       });
     }, 1);
-
   },
+
   scrollValue: 0,
-  scrollContainerHeight: HEIGHT * 1.2, //Gets calculated on scroll, but if you havent scrolled needs an initial value
-  scrollAnimation: function() {
+
+  // Gets calculated on scroll, but if you havent scrolled needs an initial value
+  scrollContainerHeight: HEIGHT,
+
+  scrollAnimation() {
     if (this.isMounted() && this.state.active) {
-      if (this.moveY == undefined) return this.requestAnimationFrame(this.scrollAnimation);
+      if (this.moveY == undefined) {
+        return this.requestAnimationFrame(this.scrollAnimation);
+      }
 
       let SCROLL_OFFSET = this.wrapperLayout.pageY;
       let moveY = this.moveY - SCROLL_OFFSET;
@@ -218,19 +289,17 @@ var SortableListView = React.createClass({
       }
       if (newScrollValue !== null) {
         this.scrollValue = newScrollValue;
-         //this.scrollResponder.scrollWithoutAnimationTo(this.scrollValue, 0);
-         this.scrollResponder.scrollTo({y: this.scrollValue, x: 0, animated: false});
+        this.scrollResponder.scrollTo({y: this.scrollValue, x: 0, animated: false});
       }
       this.checkTargetElement();
       this.requestAnimationFrame(this.scrollAnimation);
     }
   },
+
   checkTargetElement() {
     let scrollValue = this.scrollValue;
-
     let moveY = this.moveY;
     let targetPixel = scrollValue + moveY - this.firstRowY;
-
     let i = 0;
     let x = 0;
     let row;
@@ -249,105 +318,121 @@ var SortableListView = React.createClass({
     if (!isLast) x--;
     if (x != this.state.hovering) {
       LayoutAnimation.easeInEaseOut();
-      this._previouslyHovering = this.state.hovering;
-      this.__activeY = this.panY;
+
       this.setState({
-        hovering: String(x)
+        hovering: true,
+        hoverIndex: x
       })
     }
-
   },
+
   firstRowY: undefined,
   layoutMap: {},
   _rowRefs: {},
-  handleRowActive: function(row) {
+
+  handleRowActive(row) {
     if (this.props.disableSorting) return;
     this.state.pan.setValue({x: 0, y: 0});
     LayoutAnimation.easeInEaseOut();
     this.moveY = row.layout.pageY;
     this.setState({
       active: row,
-      hovering: row.rowData.index,
-    },  this.scrollAnimation);
-
+      hovering: true,
+      hoverIndex: row.rowData.index
+    }, this.scrollAnimation);
   },
-  renderActiveDivider: function() {
-    let height = this.state.active ? this.state.active.layout.frameHeight : null
-    if (this.props.renderActiveDivider) return this.props.renderActiveDivider(height);
-    return <View style={{height: height}} />
-  },
-  renderRow: function(data, section, index, highlightfn, active) {
 
+  renderActiveDivider() {
+    let height = this.state.active ? this.state.active.layout.frameHeight : null;
+
+    if (this.props.renderActiveDivider) {
+      return this.props.renderActiveDivider(height);
+    }
+    return <View style={{height}} />
+  },
+
+  renderRow(data, section, index, highlightfn, active) {
     let Component = active ? SortRow : Row;
     let isActiveRow = (!active && this.state.active && this.state.active.rowData.index === index);
+
     if (!active && isActiveRow) {
       active = {active: true};
     }
-    let hoveringIndex = this.order[this.state.hovering];
-    return (<Component
-      {...this.props}
-      activeDivider={this.renderActiveDivider()}
-      key={index}
-      active={active}
-      list={this}
-      ref={view => { this._rowRefs[active ? 'ghost' : index] = view; }}
-      hovering={hoveringIndex == index}
-      panResponder={this.state.panResponder}
-      rowData={{data, section, index}}
-      onRowActive={this.handleRowActive}
-      onRowLayout={layout => this._updateLayoutMap(index, layout.nativeEvent.layout)}
-      />);
+    let hoveringIndex = this.order[this.state.hoverIndex];
+
+    return (
+      <Component
+        {...this.props}
+        activeDivider={this.renderActiveDivider()}
+        key={index}
+        active={active}
+        list={this}
+        ref={view => { this._rowRefs[active ? 'ghost' : index] = view; }}
+        hovering={hoveringIndex == index}
+        panResponder={this.state.panResponder}
+        rowData={{data, section, index}}
+        onRowActive={this.handleRowActive}
+        onRowLayout={layout => this._updateLayoutMap(index, layout.nativeEvent.layout)}
+      />
+    );
   },
+
   _updateLayoutMap(index, layout) {
-      if (!this.firstRowY || layout.y < this.firstRowY) {
-          this.firstRowY = layout.y;
-      }
-      this.layoutMap[index] = layout;
+    if (!this.firstRowY || layout.y < this.firstRowY) {
+      this.firstRowY = layout.y;
+    }
+    this.layoutMap[index] = layout;
   },
-  renderActive: function() {
+
+  renderActive() {
     if (!this.state.active) return;
     let index = this.state.active.rowData.index;
     return this.renderRow(this.props.data[index], 's1', index, () => {}, {active: true, thumb: true});
   },
-  componentWillMount: function() {
+
+  componentWillMount() {
     this.setOrder(this.props);
   },
-  componentWillReceiveProps: function(props) {
+
+  componentWillReceiveProps(props) {
     this.setOrder(props);
   },
-  setOrder: function(props) {
-    this.order = props.order || Object.keys(props.data) || [];
+
+  setOrder(props) {
+    this.order = props.order || Object.keys(props.data);
   },
-  getScrollResponder: function() {
+
+  getScrollResponder() {
     return this.scrollResponder;
   },
-  render: function() {
-    let dataSource = this.state.ds.cloneWithRows(this.props.data, this.props.order);
 
-    return <View ref="wrapper" style={{flex: 1}} onLayout={()=>{}}>
-      <ListView
-        enableEmptySections={true}
-        {...this.props}
-        {...this.state.panResponder.panHandlers}
-        ref="list"
-        dataSource={dataSource}
-        onScroll={e => {
-          this.scrollValue = e.nativeEvent.contentOffset.y;
-          if (this.props.onScroll) this.props.onScroll(e);
-        }}
-        onContentSizeChange={(width, height) => {
-          this.scrollContainerHeight = height;
-        }}
-        onLayout={(e) => this.listLayout = e.nativeEvent.layout}
-        scrollEnabled={!this.state.active && (this.props.scrollEnabled !== false)}
-        renderRow={this.renderRow}
-      />
-      {this.renderActive()}
-    </View>
-  },
-  scrollTo: function(...args) {
-    this.scrollResponder.scrollTo.apply(this.scrollResponder, args);
+  render() {
+    let dataSource = this.state.dataSource.cloneWithRows(this.props.data, this.props.order);
+
+    return (
+      <View ref={WRAPPER_REF} style={{flex: 1}} onLayout={()=>{}}>
+        <ListView
+          renderHeader={() => this.props.header}
+          ref={LIST_REF}
+          enableEmptySections={true}
+          {...this.props}
+          {...this.state.panResponder.panHandlers}
+          dataSource={dataSource}
+          onScroll={e => {
+            this.scrollValue = e.nativeEvent.contentOffset.y;
+            if (this.props.onScroll) this.props.onScroll(e);
+          }}
+          onContentSizeChange={(width, height) => {
+            this.scrollContainerHeight = height;
+          }}
+          onLayout={(e) => this.listLayout = e.nativeEvent.layout}
+          scrollEnabled={!this.state.active && (this.props.scrollEnabled !== false)}
+          renderRow={this.renderRow}
+        />
+        {this.renderActive()}
+      </View>
+    );
   }
 });
 
-module.exports = SortableListView;
+export default SortableListView;
